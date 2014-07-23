5793ff427b3f7d655cd3958965ad36b156dabdc5
pppstudio <pppstudio.gm@gmail.com>
2014-07-23 20:14:12 +0700

    tweak deploy

--- cap_old/config/deploy/new.rb
+++ cap_old/config/deploy/new.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/var/www/apps/eorder2"
 
-set :repository,  "ssh://hexaceram/home/pppstudio/eorder2.git"
-server "hexaceram",:web,:app,:db
+#set :repository,  "ssh://hexaceram/home/pppstudio/eorder2.git"
+server "110.164.212.183",:web,:app,:db
 set :port,22
